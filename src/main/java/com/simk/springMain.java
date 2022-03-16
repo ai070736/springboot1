b05dca3d8394d38ddd4e2561ef9d5ab4cceadf08
simk <q1470970746@163.com>
2022-03-16 20:38:37 +0800

    2022/03/16-1

--- src/main/java/com/simk/springMain.java
+++ src/main/java/com/simk/springMain.java
@@ -3,7 +3,7 @@ package com.simk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-
+//测试commit1
 @EnableCaching
 @SpringBootApplication
 public class springMain {
